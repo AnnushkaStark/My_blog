155439a07792d0f44581ec11b1dc795ec2548e98
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-01-05 02:31:28 +0300

    Тестирование представления формы изменения электронной почты

--- TellYouStory/stories/test.py
+++ TellYouStory/stories/test.py
@@ -439,3 +439,56 @@ class TestChangeUserMailForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors["old_mail"], ['Обязательное поле.'])
         self.assertEqual(form.errors["new_mail"], ['Обязательное поле.'])
+
+
+class TestChangeUserMailFormView(TestCase):
+    """
+    Тестирование представление формы 
+    изменения электронной почты
+    """
+    def setUp(self):
+        """
+        Cоздание тест пользователя
+        """
+        self.client = Client()
+        self.url = reverse("user_page")
+        self.change_mail_url = reverse("change_mail")
+        self.user = User.objects.create_user(
+            username="testus", email="mytest@mail.com", password="123testpass"
+        )
+
+        self.client.login( username="testus",password="123testpass")
+    
+
+    def test_change_sucsess(self):
+        """
+        Тест успешное изменение элеткронной почты
+        """
+        data = {"old_mail": "mytest@mail.com", "new_mail": "123@testpa.ss"}
+        response = self.client.get(reverse("user_page"))
+        self.assertEqual(response.status_code,200)
+        response = self.client.post(self.change_mail_url,data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("settings_page"))
+        user = User.objects.get(email=data["new_mail"])          # Проверяем что почта  изменилась
+        self.assertEqual(user.email, data["new_mail"])
+
+    def test_change_failure(self):
+        """
+        Не успешное изменение почты
+        """
+        data = {"old_mail": "mytest@mail.com", "new_mail": "123@testpass"}
+        response = self.client.get(reverse("user_page"))
+        self.assertEqual(response.status_code,200)
+        response = self.client.post(self.change_mail_url,data,follow=True)
+        self.assertFalse(User.objects.filter(email="123@testpass").exists()) # Проверяем что почта не изменилась
+        self.assertRedirects(response, reverse("settings_page"))
+        self.assertContains(response, "Ошибка изменения данных") 
+
+
+    
+
+    
+
+
+ 
