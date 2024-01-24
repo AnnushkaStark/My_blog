e7289f0bbb29b06682f94441b8cc72843162f7ea
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-01-24 23:34:14 +0300

    Тестирование страницы пользователя с учетом вывода биографии

--- TellYouStory/stories/tests.py
+++ TellYouStory/stories/tests.py
@@ -200,14 +200,39 @@ class TestUserPageView(TestCase):
         Создание тест пользователя
         """
         self.client = Client()
+        self.user = User.objects.create_user(
+            username="tesuser", email="test@mail.ru", password="Test123#pass"
+        )
         self.url = reverse("user_page")  # Страница user
 
-    def test_login_page_view(self):
+    def test_user_page_without_bio(self):
         """
         Проверка доступности страницы user
+        и того что при переходе на страницу если у пользователя
+        нет биографии она корректно создается
         """
+        self.client.login(username="tesuser", password="Test123#pass")
         response = self.client.get(self.url)
+        self.biography = Biography(user=self.user)
+        self.biography.save()
+        biography = Biography.objects.filter(user=self.user).count()
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(biography, 1)
+        self.assertTemplateUsed(response, "user.html")
+
+    def test_user_page_with_bio(self):
+        """
+        Проверка доступности страницы и отображения биографии
+        если она уже есть
+        """
+        self.biography = Biography(user=self.user, name="Vasya", town="test_town")
+        self.biography.save()
+        self.client.login(username="tesuser", password="Test123#pass")
+        response = self.client.get(self.url)
+        biography = Biography.objects.get(user=self.user)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, biography.name)
+        self.assertContains(response, biography.town)
         self.assertTemplateUsed(response, "user.html")
 
 
