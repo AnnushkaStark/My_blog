15d83933829e84dd4b9f58498f97b012eaba7a7b
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-02-05 00:31:47 +0300

    Тестироваание доступности страницы добавления поста

--- TellYouStory/stories/tests.py
+++ TellYouStory/stories/tests.py
@@ -1265,3 +1265,35 @@ class TestStoryModel(TestCase):
         """
         self.assertEqual(self.story.author, self.user)
         self.assertEqual(self.user, self.story.author)
+
+
+
+class TestAddStoryPageView(TestCase):
+    """
+    Тестирование представления
+    страницы добавления поста
+    """
+
+    def setUp(self):
+        """
+        Cоздание тест пользователя
+        """
+        self.client = Client()
+        self.url = reverse("user_page")
+        self.add_story_url = reverse("add_story_page")
+        self.user = User.objects.create_user(
+            username="testus", email="mytest@mail.com", password="123testpassS@"
+        )
+
+        self.client.login(username="testus", password="123testpassS@")
+
+    def test_add_story_page_view(self):
+        """
+        Тестирование доступности страницы 
+        добавления поста
+        """
+        response = self.client.get(reverse("user_page"))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(self.add_story_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("add_story.html")
