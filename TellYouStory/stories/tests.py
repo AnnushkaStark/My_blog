9805f7d2a79529627552e8f2d2c13c10a69ee855
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-02-18 02:55:45 +0300

    Тестирование страниц выводва статей с упрядочиванием по ранжирванию и по хронологии

--- TellYouStory/stories/tests.py
+++ TellYouStory/stories/tests.py
@@ -34,6 +34,7 @@ from django.contrib.auth.hashers import make_password, check_password, reset_has
 from django.db.models.fields.files import ImageFieldFile
 from django.db.models.fields import DecimalField
 from django.core.exceptions import ValidationError
+from rest_framework import response
 
 
 class TestUserModel(TestCase):
@@ -1248,10 +1249,10 @@ class TestStoryModel(TestCase):
             image="test.jpg",
             content="somethingcontent",
             rank=1.0,
-            like_counter = 0,
-            dislike_counter = 0,
-            comment_counter = 0,
-            views_counter = 0,
+            like_counter=0,
+            dislike_counter=0,
+            comment_counter=0,
+            views_counter=0,
             author=self.user,
         )
 
@@ -2209,3 +2210,147 @@ class TestCommentsModel(TestCase):
         self.assertEqual(self.user, self.comment.user)
         self.assertEqual(self.comment.article, self.story)
         self.assertEqual(self.story, self.comment.article)
+
+
+class TestRankingArticlesView(TestCase):
+    """
+    Тестирование доступности страницы
+    статей с упорядчиванеим по ранжированию
+    """
+
+    def setUp(self):
+        """
+        Cоздание тест пользователя
+        """
+        self.client = Client()
+        self.url = reverse("user_page")
+        self.ranking_articles_url = reverse("ranking_articles")
+        self.user = User.objects.create_user(
+            username="testus", email="mytest@mail.com", password="123testpassS@"
+        )
+
+        self.client.login(username="testus", password="123testpassS@")
+
+        self.article_1 = Story.objects.create(
+            title="test_title_1",
+            topic="testopic_1",
+            image="test_image",
+            author=self.user,
+            like_counter=10,
+            dislike_counter=0,
+            comment_counter=10,
+            views_counter=10,
+        )
+        self.article_1.rank = self.article_1.get_rank()
+        self.article_1.save()
+        self.article_2 = Story.objects.create(
+            title="test_title_2",
+            topic="testopic_2",
+            image="test_image",
+            author=self.user,
+            like_counter=5,
+            dislike_counter=0,
+            comment_counter=5,
+            views_counter=5,
+        )
+        self.article_2.rank = self.article_1.get_rank()
+        self.article_2.save()
+        self.article_3 = Story.objects.create(
+            title="test_title_3",
+            topic="testopic_3",
+            image="test_image",
+            author=self.user,
+            like_counter=1,
+            dislike_counter=0,
+            comment_counter=1,
+            views_counter=1,
+        )
+        self.article_3.rank = self.article_1.get_rank()
+        self.article_3.save()
+
+    def test_ranking_article_page(self):
+        """
+        Проверка доступности страницы 
+        с отображением статей по
+        ранжированию
+        """
+        self.client.login(username="tesuser", password="Test123#passS")
+        response = self.client.get(self.ranking_articles_url)
+        articles = Story.objects.all().order_by("-rank")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "list_story_rank.html")
+        self.assertEqual(articles[0].title,"test_title_1" )
+        self.assertEqual(articles[1].title,"test_title_2" )
+        self.assertEqual(articles[2].title,"test_title_3" )
+
+
+class TestTimeArticlesView(TestCase):
+    """
+    Тестирование доступности страницы
+    статей с упорядчиванеим по хронологии
+    """
+
+    def setUp(self):
+        """
+        Cоздание тест пользователя
+        """
+        self.client = Client()
+        self.url = reverse("user_page")
+        self.time_articles_url = reverse("time_list_articles")
+        self.user = User.objects.create_user(
+            username="testus", email="mytest@mail.com", password="123testpassS@"
+        )
+
+        self.client.login(username="testus", password="123testpassS@")
+
+        self.article_1 = Story.objects.create(
+            title="test_title_1",
+            topic="testopic_1",
+            image="test_image",
+            author=self.user,
+            like_counter=10,
+            dislike_counter=0,
+            comment_counter=10,
+            views_counter=10,
+        )
+        self.article_1.rank = self.article_1.get_rank()
+        self.article_1.save()
+        self.article_2 = Story.objects.create(
+            title="test_title_2",
+            topic="testopic_2",
+            image="test_image",
+            author=self.user,
+            like_counter=5,
+            dislike_counter=0,
+            comment_counter=5,
+            views_counter=5,
+        )
+        self.article_2.rank = self.article_1.get_rank()
+        self.article_2.save()
+        self.article_3 = Story.objects.create(
+            title="test_title_3",
+            topic="testopic_3",
+            image="test_image",
+            author=self.user,
+            like_counter=1,
+            dislike_counter=0,
+            comment_counter=1,
+            views_counter=1,
+        )
+        self.article_3.rank = self.article_1.get_rank()
+        self.article_3.save()
+
+    def test_ranking_article_page(self):
+        """
+        Проверка доступности страницы 
+        с отображением статей по
+        ранжированию
+        """
+        self.client.login(username="tesuser", password="Test123#passS")
+        response = self.client.get(self.time_articles_url)
+        articles = Story.objects.all().order_by("-date_create")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "list_story_rank.html")
+        self.assertEqual(articles[0].title,"test_title_3" )
+        self.assertEqual(articles[1].title,"test_title_2" )
+        self.assertEqual(articles[2].title,"test_title_1" )
