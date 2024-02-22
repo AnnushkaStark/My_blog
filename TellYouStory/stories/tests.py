a020c02ab13c51ff68b4edce552bcdf05e9eb193
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-02-22 14:41:54 +0300

    Тестирование представления отображения статей опиределенного автора

--- TellYouStory/stories/tests.py
+++ TellYouStory/stories/tests.py
@@ -2400,7 +2400,7 @@ class TestTopicTimeView(TestCase):
             comment_counter=5,
             views_counter=5,
         )
-        self.article_2.rank = self.article_1.get_rank()
+        self.article_2.rank = self.article_2.get_rank()
         self.article_2.save()
         self.article_3 = Story.objects.create(
             title="test_title_3",
@@ -2412,7 +2412,7 @@ class TestTopicTimeView(TestCase):
             comment_counter=1,
             views_counter=1,
         )
-        self.article_3.rank = self.article_1.get_rank()
+        self.article_3.rank = self.article_3.get_rank()
         self.article_3.save()
      
 
@@ -2431,5 +2431,84 @@ class TestTopicTimeView(TestCase):
         self.assertEqual(articles[1].title,"test_title_1" )
         self.assertNotEqual(articles[0].title,"test_title_2" )
         self.assertNotEqual(articles[1].title,"test_title_2" )
-        self.assertNotEqual(articles[1].title,"test_title_2" )
         
+        
+
+class TestAuthorArticlesView(TestCase):
+    """
+    Тестирование представления
+    отображения статей определенного
+    автора с упорядочиванием 
+    по хронологии
+    """
+    def setUp(self):
+        """
+        Cоздание тест пользователя
+        и тест статей
+        """
+        self.client = Client()
+        self.url = reverse("user_page")
+        self.user = User.objects.create_user(
+            username="testus", email="mytest@mail.com", password="123testpassS@"
+        )
+        self.user_target = User.objects.create_user(
+            username="testus_2", email="mytest@mail.ru", password="1234testpassS@"
+        )
+
+        self.article_1 = Story.objects.create(
+            title="test_title_1",
+            topic="testopic_1",
+            image="test_image",
+            author=self.user_target,
+            like_counter=10,
+            dislike_counter=0,
+            comment_counter=10,
+            views_counter=10,
+        )
+        self.article_1.rank = self.article_1.get_rank()
+        self.article_1.save()
+
+        self.article_2 = Story.objects.create(
+            title="test_title_2",
+            topic="testopic_2",
+            image="test_image",
+            author=self.user,
+            like_counter=5,
+            dislike_counter=0,
+            comment_counter=5,
+            views_counter=5,
+        )
+        self.article_2.rank = self.article_2.get_rank()
+        self.article_2.save()
+        self.article_3 = Story.objects.create(
+            title="test_title_3",
+            topic="testopic_1",
+            image="test_image",
+            author=self.user,
+            like_counter=1,
+            dislike_counter=0,
+            comment_counter=1,
+            views_counter=1,
+        )
+        self.article_3.rank = self.article_3.get_rank()
+        self.article_3.save()
+
+        self.author_id = 2
+        self.author_article_url = reverse("authors_articles",kwargs={"author_id":self.author_id})
+
+    def test_author_article_page(self):
+        """
+        Проверка доступности страницы 
+        с отображением статей по
+        определенной тематике
+        """
+        self.client.login(username="testus", password="123testpassS@")
+        response = self.client.get(self.author_article_url)
+        articles = Story.objects.filter(author_id=self.author_id).order_by("-date_create").all()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "article_author.html")
+        self.assertEqual(articles[0].title,"test_title_1" )
+        self.assertNotEqual(articles[0].title,"test_title_2" )
+        self.assertNotEqual(articles[0].title,"test_title_3" )
+        
+
